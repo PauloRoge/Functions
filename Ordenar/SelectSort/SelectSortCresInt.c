8e5bc0e9087e2d088c54b925e3551eddc0d25462
paulo <paulo.rogerio.candido@uel.br>
2023-03-24 13:25:30 -0300

    SelectSort ok!

---
+++ Ordenar/SelectSort/SelectSortCresInt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void selectSort(int* v, int N);
+void trocaint(int* a, int* b);
+
+int main() {
+
+	int vetor[] = { 2,4,6,8,10,1,3,5,7,9 };
+
+	selectSort(vetor, 10);
+
+	for (int i = 0; i < 10; i++)
+		printf("%i ", vetor[i]);
+
+	return 0;
+}
+
+void selectSort(int* v, int N)
+{
+	for (int j = 0; j < N; j++)
+	{
+		for (int i = j + 1; i < N; i++)
+		{
+			if (v[i] < v[j]) //para tornar decrecente inverter para >
+				trocaint(&v[i], &v[j]);
+		}
+	}
+}
+
+void trocaint(int* a, int* b)
+{
+	int aux = 0;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
