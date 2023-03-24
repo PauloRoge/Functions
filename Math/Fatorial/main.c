a55ecb510d78b12631c2ef89f1b9d5f4333b36ec
paulo <paulo.rogerio.candido@uel.br>
2023-03-24 19:37:48 -0300

    fatorial ok

--- Math/Fatorial/main.c
+++ Math/Fatorial/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
-int fatorial(int n);
+long long int fatorial(int n);
+long long int fatorialRecursivo(int n);
 
 int main(void)
 {
 	int numero = 10;
 
-	printf("%d", fatorial(10));
+	printf("%lli", fatorialRecursivo(4));
 }
 
-fatorial(int n)
+long long int fatorial(int n)
 {
 	if (n == 0)
 		return 0;
 
 	return n + fatorial(n - 1);
+}
+
+long long int fatorialRecursivo(int n)
+{
+	if (n == 2)
+		return 2;
+
+	return n * fatorialRecursivo(n - 1);
 }
