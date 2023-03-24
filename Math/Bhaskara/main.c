44a4a4a622334ba9d76d248497df8ba65ee6b3fc
paulo <paulo.rogerio.candido@uel.br>
2023-03-24 14:51:45 -0300

    bhaskara ok

--- Math/Bhaskara/main.c
+++ Math/Bhaskara/main.c
@@ -9,41 +9,39 @@ int main() {
     double a = 0, b = 0, c = 0;
     double x1=0, x2=0;
     double delta = bhaskara(a, b, c, &x1, &x2);
-
     //-------------------------------------
+
     char parametro[3] = { 'a', 'b', 'c' };
+    int continuar = 1;
 
-    for (int i = 0; i < 3; i++)
+    while (continuar)
     {
-        printf("informe %c: ", i < 1 ? parametro[0] : i == 1 ? parametro[1] : parametro[2]);
-        scanf_s(" %lf", i < 1 ? &a : i == 1 ? &b : &c);
+        system("cls");
+        for (int i = 0; i < 3; i++)
+        {
+            printf("informe %c: ", i < 1 ? parametro[0] : i == 1 ? parametro[1] : parametro[2]);
+            scanf_s(" %lf", i < 1 ? &a : i == 1 ? &b : &c);
+        }
+        delta = bhaskara(a, b, c, &x1, &x2);
+
+        printf("\n\tx1 = %.2f\tx2 = %.2f\n\n", x1, x2);
+        printf("Valor de delta: %.2f\n", delta);
+        printf("\n\t(1)novamente (0)sair");
+        continuar=getch();
     }
-    system("cls");
-
-    delta = bhaskara(a, b, c, &x1, &x2);
-
-    printf("Raizes da equacao %.2fx^2 + %.2fx + %.2f:\n", a, b, c);
-    printf("\n\tx1 = %.2f\tx2 = %.2f\n\n", x1, x2);
-    printf("Valor de delta: %.2f\n", delta);
+    
     return 0;
 }
 
 double bhaskara(double a, double b, double c, double* x1, double* x2) {
     double delta = b * b - 4 * a * c;
 
-    if (a == 0)// Verifica se a é zero para evitar divisão por zero, se a=0 não é 2 grau.
-    {
-        *x1 = *x2 = 0;// Atribui zero aos valores das raízes
-        return 0;// colocar NULL
-    }
-       
-
-    if (delta < 0) { // Não há raízes reais
+    if (delta < 0) // Não há raízes reais
         *x1 = *x2 = 0; // Atribui zero aos valores das raízes
-    }
-    else if (delta == 0) { // Há apenas uma raiz real
+
+    else if (delta == 0) // Há apenas uma raiz real
         *x1 = *x2 = -b / (2 * a);
-    }
+
     else { // Há duas raízes reais
         *x1 = (-b + sqrt(delta)) / (2*a);
         *x2 = (-b - sqrt(delta)) / (2*a);
